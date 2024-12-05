598818995c69214a3312dd89cec367e9f2bbb786
Gabriel Barboza <gabrielgbarbozadev@gmail.com>
2024-12-05 13:00:27 -0300

    habilitei a função do botão e alteração do fundo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-alert("Ola Mundo");
+function alterarStatus(numero) {
+  let textoBotao = document.querySelector(`#game-${numero} a`);
+  let capaJogo = document.querySelector(`#game-${numero} div`);
+  if (textoBotao.innerHTML == "Alugar") {
+    textoBotao.innerHTML = "Devolver";
+    textoBotao.classList.add("dashboard__item__button--return");
+    capaJogo.classList.add("dashboard__item__img--rented")
+  } else {
+    textoBotao.innerHTML = "Alugar";
+    textoBotao.classList.remove("dashboard__item__button--return");
+    capaJogo.classList.remove("dashboard__item__img--rented");
+  }
+}
+
+
+  
+  
+  
+  
+  
+  
