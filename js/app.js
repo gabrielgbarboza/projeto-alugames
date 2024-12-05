7bbadc280486bf4dfa1c90101b8c3b99f1a8d2f9
Gabriel Barboza <gabrielgbarbozadev@gmail.com>
2024-12-05 13:24:08 -0300

    Adicionei comentários comparando as soluções

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// minha solução
 function alterarStatus(numero) {
   let textoBotao = document.querySelector(`#game-${numero} a`);
   let capaJogo = document.querySelector(`#game-${numero} div`);
@@ -12,6 +13,23 @@ function alterarStatus(numero) {
   }
 }
 
+//solução alura
+// function alterarStatus(id) {
+//     let gameClicado = document.getElementById(`game-${id}`);
+//     let imagem = gameClicado.querySelector('.dashboard__item__img');
+//     let botao = gameClicado.querySelector('.dashboard__item__button');
+   
+//     if (imagem.classList.contains('dashboard__item__img--rented')) {
+//             imagem.classList.remove('dashboard__item__img--rented');
+//             botao.classList.remove('dashboard__item__button--return');
+//             botao.textContent = 'Alugar';
+//         }
+//      else {
+//         imagem.classList.add('dashboard__item__img--rented');
+//         botao.classList.add('dashboard__item__button--return');
+//         botao.textContent = 'Devolver';
+//     }
+// }
 
   
   
